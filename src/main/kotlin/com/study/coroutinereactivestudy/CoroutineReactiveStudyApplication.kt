b21a18d674aa6e188261c306d9b22937ab2cec4b
icarus8050 <icarus8050@naver.com>
2022-04-22 10:52:19 +0900

    init

---
+++ src/main/kotlin/com/study/coroutinereactivestudy/CoroutineReactiveStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.study.coroutinereactivestudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutineReactiveStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoroutineReactiveStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/coroutinereactivestudy/CoroutineReactiveStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.coroutinereactivestudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutineReactiveStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
