7a0eef2642bb0375c53dd3a8e45e4b55691bef23
icarus8050 <icarus8050@naver.com>
2022-05-01 00:16:42 +0900

    complete and completeExceptionally

---
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_2/Example_19.kt
@@ -0,0 +1,30 @@
+package com.study.coroutinereactivestudy.coroutine_deep_dive.part_2
+
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val job = Job()
+
+    launch(job) {
+        repeat(5) { num ->
+            delay(200)
+            println("Repeat num : $num")
+        }
+    }
+
+    launch {
+        delay(500)
+        job.complete()
+    }
+
+    job.join()
+
+    launch(job) {
+        println("Will not be printed")
+    }
+
+    println("Done")
+}
