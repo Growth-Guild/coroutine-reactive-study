102a913c8d1c0050a8d9d9ea843828773b8920dd
icarus8050 <icarus8050@naver.com>
2022-05-03 02:09:43 +0900

    cancellation NonCancellable

---
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_2/Example_25.kt
@@ -0,0 +1,28 @@
+package com.study.coroutinereactivestudy.coroutine_deep_dive.part_2
+
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+suspend fun main(): Unit = coroutineScope {
+    val job = Job()
+    launch(job) {
+        try {
+            delay(200)
+            println("Coroutine finished")
+        } finally {
+            println("Finally")
+            withContext(NonCancellable) {
+                delay(1000L)
+                println("Cleanup done")
+            }
+        }
+    }
+    delay(100)
+    job.cancelAndJoin()
+    println("Done")
+}
