9c1941cf895e374b4454544f42342749b305881f
icarus8050 <icarus8050@naver.com>
2022-05-07 00:53:39 +0900

    coroutineScope children

---
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_2/Example_40.kt
@@ -0,0 +1,23 @@
+package com.study.coroutinereactivestudy.coroutine_deep_dive.part_2
+
+import kotlinx.coroutines.*
+
+suspend fun longTask() = coroutineScope {
+    launch {
+        delay(1000)
+        val name = coroutineContext[CoroutineName]?.name
+        println("[$name] Finished task 1")
+    }
+
+    launch {
+        delay(2000)
+        val name = coroutineContext[CoroutineName]?.name
+        println("[$name] Finished task 2")
+    }
+}
+
+fun main() = runBlocking(CoroutineName("Parent")) {
+    println("Before")
+    longTask()
+    println("After")
+}
