58d0ab2b7bc71ab891af3633a7893b6757759bc7
icarus8050 <icarus8050@naver.com>
2022-05-19 10:11:05 +0900

    collection and sequence (blocking)

---
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_17.kt
@@ -0,0 +1,33 @@
+package com.study.coroutinereactivestudy.coroutine_deep_dive.part_3
+
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
+
+private fun getList(): List<String> = List(3) {
+    Thread.sleep(1000)
+    "User $it"
+}
+
+private fun getSequence(): Sequence<String> = sequence {
+    repeat(3) {
+        Thread.sleep(1000)
+        yield("User $it")
+    }
+}
+
+@DelicateCoroutinesApi
+suspend fun main() {
+    withContext(newSingleThreadContext("main")) {
+        launch {
+            repeat(3) {
+                delay(100)
+                println("Processing on coroutine")
+            }
+        }
+        val list = getSequence()
+        list.forEach { println(it) }
+    }
+}
