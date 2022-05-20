1e81b18cd5066cdf9e212e1e8c8accb51398ea1b
icarus8050 <icarus8050@naver.com>
2022-05-21 03:16:33 +0900

    simple flow builder

--- src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_19.kt
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_19.kt
@@ -1,5 +1,8 @@
 package com.study.coroutinereactivestudy.coroutine_deep_dive.part_3
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOf
 
@@ -11,4 +14,27 @@ suspend fun main() {
 
     emptyFlow<Int>()
         .collect { println(it) }
+
+    println()
+
+    listOf(1, 2, 3, 4, 5)
+        .asFlow()
+        .collect { println(it) }
+
+    val function = suspend {
+        delay(1000)
+        "UserName"
+    }
+
+    function.asFlow()
+        .collect { println(it) }
+
+    ::getUserName
+        .asFlow()
+        .collect { println(it) }
+}
+
+private suspend fun getUserName(): String {
+    delay(1000)
+    return "UserName"
 }
