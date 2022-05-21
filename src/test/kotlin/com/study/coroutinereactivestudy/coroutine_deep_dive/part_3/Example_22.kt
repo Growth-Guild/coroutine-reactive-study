493635b988bd56e09e82f1001a19bd48da85e770
icarus8050 <icarus8050@naver.com>
2022-05-21 18:45:21 +0900

    hot data stream on flow example

---
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_22.kt
@@ -0,0 +1,47 @@
+package com.study.coroutinereactivestudy.coroutine_deep_dive.part_3
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+
+private data class User(val name: String)
+
+private interface UserApi {
+    suspend fun takePage(pageNumber: Int): List<User>
+}
+
+private class FakeUserApi : UserApi {
+    private val users = List(20) { User("User $it") }
+    private val pageSize: Int = 3
+
+    override suspend fun takePage(pageNumber: Int): List<User> {
+        delay(1000)
+        return users
+            .drop(pageSize * pageNumber)
+            .take(pageSize)
+    }
+}
+
+private fun allUsersFlow(api: UserApi): Flow<User> = flow {
+    var page = 0
+    do {
+        println("Fetching page $page")
+        val users = api.takePage(page++)
+        emitAll(users.asFlow())
+    } while (!users.isNullOrEmpty())
+}
+
+suspend fun main() {
+    val api = FakeUserApi()
+    val users = allUsersFlow(api)
+    val user = users
+        .first {
+            println("Checking $it")
+            delay(1000)
+            it.name == "User 3"
+        }
+    println(user)
+}
