f47e92cc7a54b29f8ccab0c42761c77a01750b31
icarus8050 <icarus8050@naver.com>
2022-05-25 07:39:52 +0900

    flowOn

---
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_28.kt
@@ -0,0 +1,30 @@
+package com.study.coroutinereactivestudy.coroutine_deep_dive.part_3
+
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.coroutineContext
+
+suspend fun present(place: String, message: String) {
+    val ctx = coroutineContext
+    val name = ctx[CoroutineName]?.name
+    println("[$name] $message on $place")
+}
+
+fun messagesFlow(): Flow<String> = flow {
+    present("flow builder", "Message")
+    emit("Message")
+}
+
+suspend fun main() {
+    val messages = messagesFlow()
+    withContext(CoroutineName("Name1")) {
+        messages.flowOn(CoroutineName("Name3"))
+            .onEach { present("onEach", it) }
+            .flowOn(CoroutineName("Name2"))
+            .collect { present("collect", it) }
+    }
+}
