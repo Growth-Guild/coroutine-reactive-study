a5ccad835242f17c1f255ce0802177320a59ba7c
icarus8050 <icarus8050@naver.com>
2022-05-25 07:58:44 +0900

    launchIn

---
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_29.kt
@@ -0,0 +1,14 @@
+package com.study.coroutinereactivestudy.coroutine_deep_dive.part_3
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+
+suspend fun main(): Unit = coroutineScope {
+    flowOf("User1", "User2")
+        .onStart { println("Users:") }
+        .onEach { println(it) }
+        .launchIn(this)
+}
