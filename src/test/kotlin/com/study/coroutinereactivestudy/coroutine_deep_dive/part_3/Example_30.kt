518e82b9e93c2f8604bcccdf509f3a51144c7930
icarus8050 <icarus8050@naver.com>
2022-05-27 08:44:49 +0900

    map, filter, take, drop

--- src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_30.kt
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_30.kt
@@ -2,26 +2,28 @@ package com.study.coroutinereactivestudy.coroutine_deep_dive.part_3
 
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.take
 
 suspend fun main() {
-    flowOf(1, 2, 3)
+    flowOf(1, 2, 3, 4)
         .map { it * it }
-        .collect { print(it) }
+        .filter { it % 2 == 0 }
+        .collect { println(it) }
 
     println()
 
     ('A'..'Z').asFlow()
         .take(5)
-        .collect { print(it) }
+        .collect { println(it) }
 
     println()
 
     ('A'..'Z').asFlow()
         .drop(20)
-        .collect { print(it) }
+        .collect { println(it) }
 
     println()
 }
