92b2749071d552675d0c7de6bb4a6d8e426222d7
icarus8050 <icarus8050@naver.com>
2022-05-27 00:23:40 +0900

    flow take and drop

--- src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_30.kt
+++ src/test/kotlin/com/study/coroutinereactivestudy/coroutine_deep_dive/part_3/Example_30.kt
@@ -1,10 +1,27 @@
 package com.study.coroutinereactivestudy.coroutine_deep_dive.part_3
 
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.take
 
 suspend fun main() {
     flowOf(1, 2, 3)
         .map { it * it }
-        .collect { print(it)}
+        .collect { print(it) }
+
+    println()
+
+    ('A'..'Z').asFlow()
+        .take(5)
+        .collect { print(it) }
+
+    println()
+
+    ('A'..'Z').asFlow()
+        .drop(20)
+        .collect { print(it) }
+
+    println()
 }
